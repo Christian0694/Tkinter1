2bf0aea12d41c01090c48120d48927c964856cbb
Christian0694 <christian0694@gmail.com>
2024-10-01 23:10:36 -0400

    tercer commit

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 from tkinter import *
 
 root = Tk() #crea el main window
+
+myLabel = Label(root, text="Hello World!") #crea un label, igual que en windows forms
+myLabel2 = Label(root, text="Segunda linea")
+myLabel3 = Label(root, text="Segunda linea")
+
+#myLabel.pack() #muestra en pantalla ajustado al label
+
+
+myLabel.grid(row=0, column=1)
+myLabel2.grid(row=0, column=1)
+myLabel3.grid(row=0, column=1)
+
+
+
+
+
+root.mainloop() #event loop
